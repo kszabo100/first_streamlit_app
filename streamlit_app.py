335a40cb4140523e54441a3c2132388bbd077e90
kszabo100 <109389615+kszabo100@users.noreply.github.com>
2022-07-21 20:14:46 -0700

    Update streamlit_app.py
    moved import stmts to top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+
 
 my_fruit_list = pandas.read_csv("http://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -27,7 +32,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # removing: # streamlit.text(fruityvice_response.json())  #just writes teh data to the screen
 
@@ -41,7 +45,6 @@ streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
