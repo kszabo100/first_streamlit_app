b351a1a48c7cd52c924e3634636c1e66dd51996e
kszabo100 <109389615+kszabo100@users.noreply.github.com>
2022-07-21 20:13:15 -0700

    Update streamlit_app.py
    added a stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
