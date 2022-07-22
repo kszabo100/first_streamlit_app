8edb1eb387449410d3a2c70e9da3b48cb69b2b3c
kszabo100 <109389615+kszabo100@users.noreply.github.com>
2022-07-22 14:16:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ import snowflake.connector
 from urllib.error import URLError
 
 
-
+## fruit-list from an online document
 my_fruit_list = pandas.read_csv("http://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfaxt Menu')
@@ -29,16 +31,13 @@ streamlit.dataframe(fruits_to_show)
 
 ####################################################################
 
-# create a repeatable code block (called a function)
+
+# create a function - call to a URL 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  # take the json version of the response and normalize it
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # output it on the screen as a table
-  return fruityvice_normalized
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  # take the json version of the response and normalize it
+  return fruityvice_normalized  # output it on the screen as a table
 
-
-#New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -47,8 +46,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-    
-    #streamlit.write('The user entered ', fruit_choice)
+    # streamlit.write('The user entered ', fruit_choice)
 
 except URLError as e:
   streamlit.error()
@@ -56,10 +54,8 @@ except URLError as e:
 
 
 
-
-
 streamlit.header("View Our Fruit List - Add Your Favorites!")
-# Snowflake-related functions
+# function to Snowflake - 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
